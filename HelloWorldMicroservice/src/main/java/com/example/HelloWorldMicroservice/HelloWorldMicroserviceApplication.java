0c77135ea7d52a88cba8e6d191211ce4fcd31ad4
fesabelilla <fesabelilla@gmail.com>
2022-03-26 21:18:13 +0600

    Hello world microservice init

---
+++ HelloWorldMicroservice/src/main/java/com/example/HelloWorldMicroservice/HelloWorldMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.HelloWorldMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldMicroserviceApplication.class, args);
+	}
+
+}
---
+++ HelloWorldMicroservice/src/test/java/com/example/HelloWorldMicroservice/HelloWorldMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HelloWorldMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
